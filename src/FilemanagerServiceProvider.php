1f7843cdb3d4c0abb7cc0b5d93077f9ce743885b
Ravi Mehta <rvmehta745@gmail.com>
2018-07-02 14:32:04 +0530

    first commit

---
+++ src/FilemanagerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Spider\Filemanager;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilemanagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
